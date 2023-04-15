463b307ee78ef8b853addcabec545ccfd896cc8e
junghoon <111417407+pobiq@users.noreply.github.com>
2023-04-15 21:19:16 +0900

    [Silver III] Title: N과 M (5), Time: 2460 ms, Memory: 56116 KB -BaekjoonHub

---
+++ 백준/Silver/15654. N과 M （5）/N과 M （5）.java
@@ -0,0 +1,66 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.StringTokenizer;
+
+public class Main {
+	
+	static int M, N;
+	static int[] array;
+	static boolean[] visited;
+	
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		StringBuffer sb = new StringBuffer();
+		
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		
+		N = Integer.parseInt(st.nextToken());
+		M = Integer.parseInt(st.nextToken());
+		
+		array = new int[N];
+		visited = new boolean[N];
+		
+		st = new StringTokenizer(br.readLine());
+		
+		for(int i=0; i<N; i++) {
+			array[i] = Integer.parseInt(st.nextToken());
+		}
+		
+		Arrays.sort(array);
+		
+		dfs(0, new int[M]);
+		
+		bw.write(sb.toString());
+		bw.flush();
+		bw.close();
+	}
+	
+	static void dfs(int depth, int[] output) {
+		
+		if(depth == M) {
+			for(int i : output) {
+				System.out.print(i + " ");
+			}
+			System.out.println();
+			return;
+		}
+		
+		for(int i=0; i<N; i++) {
+			if(!visited[i]) {
+				visited[i] = true;
+				output[depth] = array[i];
+				dfs(depth+1, output);
+				visited[i] = false;
+			}
+		}
+		
+	}
+}

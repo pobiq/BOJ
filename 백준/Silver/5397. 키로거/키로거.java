ffde6f2ee93a66165c873b1a9c5b831c5e02ceb3
junghoon <111417407+pobiq@users.noreply.github.com>
2023-05-07 13:04:33 +0900

    [Silver II] Title: 키로거, Time: 724 ms, Memory: 122960 KB -BaekjoonHub

--- 백준/Silver/5397. 키로거/키로거.java
+++ 백준/Silver/5397. 키로거/키로거.java
@@ -1,62 +1,42 @@
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
-import java.util.LinkedList;
-import java.util.ListIterator;
+import java.io.*;
+import java.util.*;
 
 public class Main {
-	
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-		StringBuffer sb = new StringBuffer();
-		
-		int n = Integer.parseInt(br.readLine());
-		
-		while(n --> 0) {
-			LinkedList<Character> linkList = new LinkedList<>();
-			ListIterator<Character> iterator = linkList.listIterator();
-			StringBuffer sub = new StringBuffer();
-			
-			String str = br.readLine();
-			char[] order = str.toCharArray();
-			for(char c : order) {
-				switch(c) {
-				case '<':
-					if(iterator.hasPrevious()) {
-						iterator.previous();
-					}
-					break;
-				case '>':
-					if(iterator.hasNext()) {
-						iterator.next();
-					}
-					break;
-				case '-':
-					if(iterator.hasPrevious()) {
-						iterator.previous();
-						iterator.remove();
-					}
-					break;
-				default:
-					iterator.add(c);
-					break;
-				}
-			}
-			
-			for(char c : linkList) {
-				sub.append(c);
-			}
-			
-			sb.append(sub.toString()).append("\n");
-			
-		}
-		
-		bw.write(sb.toString());
-		bw.flush();
-		bw.close();
-	}
-	
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw =  new BufferedWriter(new OutputStreamWriter(System.out));
+        StringBuilder sb = new StringBuilder();
+        int n = Integer.parseInt(br.readLine());
+        for (int i = 0; i < n; i++) {
+            String str = br.readLine();
+            int l = str.length();
+            List<Character> list = new LinkedList<>();
+            ListIterator<Character> iterator = list.listIterator();
+            for (int j = 0; j < l; j++) {
+                char c = str.charAt(j);
+                if (c == '<') { // 왼쪽 커서 화살표
+                    if (!iterator.hasPrevious()) continue;
+                    iterator.previous();
+
+                } else if (c == '>') { // 오른쪽 커서 화살표
+                    if (!iterator.hasNext()) continue;
+                    iterator.next();
+
+                } else if (c == '-') { // 백스페이스
+                    if (!iterator.hasPrevious()) continue;
+                    iterator.previous();
+                    iterator.remove();
+                } else { // 일반 문자
+                    iterator.add(c);
+                }
+            }
+            
+            for(char c : list) {
+            	sb.append(c);
+            }
+            sb.append("\n");
+        }
+        bw.write(sb.toString());
+        bw.close();
+    }
 }

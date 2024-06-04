c52d15e234e8d1ef9be784b592c3a39167d4b516
junghoon <111417407+pobiq@users.noreply.github.com>
2024-06-04 14:00:38 +0900

    [Silver I] Title: 그림, Time: 400 ms, Memory: 44072 KB -BaekjoonHub

--- 백준/Silver/1926. 그림/그림.java
+++ 백준/Silver/1926. 그림/그림.java
@@ -8,83 +8,98 @@ import java.util.Queue;
 import java.util.StringTokenizer;
 
 public class Main {
-	
-	static int n, m;
-	static int[][] map;
-	static boolean[][] visited;
-	static int photo = 0;
-	static int max = 0;
-	static int[] dx = {0, 0, -1, 1};
-	static int[] dy = {-1, 1, 0, 0};
 
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-		StringBuffer sb = new StringBuffer();
-		
-		
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		n = Integer.parseInt(st.nextToken());
-		m = Integer.parseInt(st.nextToken());
-		map = new int[n][m];
-		visited = new boolean[n][m];
-		
-		for(int i=0; i<n; i++) {
-			st = new StringTokenizer(br.readLine());
-			for(int j=0; j<m; j++) {
-				map[i][j] = Integer.parseInt(st.nextToken());
-			}
-		}
-		
-		for(int i=0; i<n; i++) {
-			for(int j=0; j<m; j++) {
-				if(!visited[i][j] && map[i][j] == 1) {
-					visited[i][j] = true;
-					bfs(i, j);
-					photo++;
-				}
-			}
-		}
-		
-		sb.append(photo).append("\n");
-		sb.append(max);
-		
-		bw.write(sb.toString());
-		bw.flush();
-		bw.close();
-	}
-	
-	static void bfs(int a, int b) {
-		Queue<int[]> queue = new LinkedList<>();
-		queue.add(new int[] {a, b});
-		
-		int area = 0;
-		
-		while(!queue.isEmpty()) {
-			int[] temp = queue.poll();
-			int x = temp[0];
-			int y = temp[1];
-			
-			area++;
-			
-			for(int i=0; i<4; i++) {
-				int xx = x + dx[i];
-				int yy = y + dy[i];
-				
-				if(xx < 0 || xx >= n || yy < 0 || yy >= m) continue;
-				
-				if(visited[xx][yy] || map[xx][yy] == 0) continue;
-				
-				visited[xx][yy] = true;
-				queue.add(new int[] {xx, yy});
-				
-			}
-		}
-		
-		if(max < area) {
-			max = area;
-		}
-		
-	}
-	
+  static int n,m;
+  static int[][] map;
+  static boolean[][] visited;
+
+  static int[] dx = {-1, 1, 0, 0};
+
+  static int[] dy = {0, 0, -1, 1};
+
+  public static class Node {
+    int x;
+    int y;
+
+    public Node(int x, int y) {
+      this.x = x;
+      this.y = y;
+    }
+  }
+
+  static int photoCount = 0;
+  static int maxArea = 0;
+
+  public static void main(String[] args) throws IOException {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+    StringBuffer sb = new StringBuffer();
+
+    StringTokenizer st = new StringTokenizer(br.readLine());
+
+    n = Integer.parseInt(st.nextToken());
+    m = Integer.parseInt(st.nextToken());
+
+    map = new int[n][m];
+    visited = new boolean[n][m];
+
+    for (int i = 0; i < n; i++) {
+      StringTokenizer st1 = new StringTokenizer(br.readLine());
+      for (int j = 0; j < m; j++) {
+        map[i][j] = Integer.parseInt(st1.nextToken());
+      }
+    }
+
+    for (int i = 0; i < n; i++) {
+      for (int j = 0; j < m; j++) {
+        if(map[i][j] == 1 && !visited[i][j]) {
+          bfs(i, j);
+        }
+      }
+    }
+
+    sb.append(photoCount).append("\n");
+    sb.append(maxArea);
+
+    bw.write(sb.toString());
+    bw.flush();
+    bw.close();
+  }
+
+  public static void bfs(int a, int b) {
+    Queue<Node> queue = new LinkedList<>();
+    queue.add(new Node(a, b));
+    int area = 0;
+    visited[a][b] = true;
+    photoCount++;
+
+    while(!queue.isEmpty()) {
+      Node node = queue.poll();
+      int x = node.x;
+      int y = node.y;
+      area++;
+
+      for(int i = 0; i < 4; i++) {
+        int nextX = x + dx[i];
+        int nextY = y + dy[i];
+
+        if(nextX < 0 || nextX >= n || nextY < 0 || nextY >= m) continue;
+
+        if(visited[nextX][nextY]) continue;
+
+        if(map[nextX][nextY] == 0) continue;
+
+        visited[nextX][nextY] = true;
+        queue.add(new Node(nextX, nextY));
+
+      }
+
+    }
+
+    maxArea = Math.max(maxArea, area);
+
+  }
+
+
+
 }

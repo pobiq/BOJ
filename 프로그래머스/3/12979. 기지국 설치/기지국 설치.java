79de5d60704d38efe8e89d1f52bdc2bcd6948001
junghoon <111417407+pobiq@users.noreply.github.com>
2024-09-19 18:15:13 +0900

    [level 3] Title: 기지국 설치, Time: 0.62 ms, Memory: 53.1 MB -BaekjoonHub

--- 프로그래머스/3/12979. 기지국 설치/기지국 설치.java
+++ 프로그래머스/3/12979. 기지국 설치/기지국 설치.java
@@ -1,24 +1,32 @@
-public class Solution {
+class Solution {
   public int solution(int n, int[] stations, int w) {
     int answer = 0;
 
     int start = 1;
     int end = 1;
+    int width = 2 * w + 1;
 
     for(int station : stations) {
       end = station - w - 1;
+      if(end < start) {
+        start = station + w + 1;
+        continue;
+      }
       int length = end - start + 1;
-      answer += (int) Math.ceil((double) length / (2 * w + 1));
+
+      answer += length / width;
+      if(length % width != 0) answer++;
+
       start = station + w + 1;
     }
 
     if(start <= n) {
       end = n;
       int length = end - start + 1;
-      answer += (int) Math.ceil((double) length / (2 * w + 1));
+      answer += length / width;
+      if(length % width != 0) answer++;
     }
 
     return answer;
   }
-
 }

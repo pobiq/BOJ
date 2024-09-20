9e6d6ed1034d196100095af356f8a18cd51ca979
junghoon <111417407+pobiq@users.noreply.github.com>
2024-09-20 16:18:26 +0900

    [Gold IV] Title: 최단경로, Time: 756 ms, Memory: 111948 KB -BaekjoonHub

--- 백준/Gold/1753. 최단경로/최단경로.java
+++ 백준/Gold/1753. 최단경로/최단경로.java
@@ -1,95 +1,96 @@
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 import java.util.*;
 
 public class Main {
-	
-	static class Edge implements Comparable<Edge> {
-		int to;
-		int weight;
-		
-		public Edge(int to, int weight) {
-			this.to = to;
-			this.weight = weight;
-		}
-		
-		@Override
-		public int compareTo(Edge o) {
-			return this.weight - o.weight;
-		}
-		
-	}
-	
-	static int v, e;
-	static int k;
-	static List<Edge>[] graph;
-	static int[] dist;
-	
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-		StringBuffer sb = new StringBuffer();
-		
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		v = Integer.parseInt(st.nextToken());
-		e = Integer.parseInt(st.nextToken());
-		
-		graph = new List[v+1];
-		dist = new int[v+1];
-		
-		int result = 0;
-		
-		for(int i=1; i<=v; i++) {
-			graph[i] = new ArrayList<>();
-			dist[i] = Integer.MAX_VALUE;
-		}
-		
-		k = Integer.parseInt(br.readLine());
-		
-		for(int i=0; i<e; i++) {
-			st = new StringTokenizer(br.readLine());
-			int now = Integer.parseInt(st.nextToken());
-			int to = Integer.parseInt(st.nextToken());
-			int weight = Integer.parseInt(st.nextToken());
-			graph[now].add(new Edge(to, weight));
-		}
-		
-		dijkstra(k);
-		
-		for(int i=1; i<=v; i++) {
-			if(dist[i] == Integer.MAX_VALUE) {
-				sb.append("INF");
-			} else {
-				sb.append(dist[i]);
-			}
-			sb.append("\n");
-		}
-		
-		bw.write(sb.toString());
-		bw.flush();
-		bw.close();
-	}
-	
-	static void dijkstra(int k) {
-		PriorityQueue<Edge> pq = new PriorityQueue<>();
-        pq.add(new Edge(k, 0));
-        dist[k] = 0;
-        
-        while(!pq.isEmpty()) {
-        	Edge e = pq.poll();
-        	int now = e.to;
-        	int weight = e.weight;
-        	
-        	if(dist[now] < weight) continue;
-        	
-        	for(Edge edge : graph[now]) {
-        		int cost = weight + edge.weight;
-        		if(cost < dist[edge.to]) {
-        			dist[edge.to] = cost;
-        			pq.add(new Edge(edge.to, cost));
-        		}
-        	}
-        	
+
+  static int V, E;
+  static int start;
+  static ArrayList<Node>[] graph;
+  static int[] dist;
+
+  static class Node {
+    int index;
+    int cost;
+
+    Node(int index, int cost) {
+      this.index = index;
+      this.cost = cost;
+    }
+
+    @Override
+    public String toString() {
+      return "(" + this.index + ", " + this.cost + ")";
+    }
+
+  }
+
+  public static void main(String[] args) throws IOException {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+    StringBuffer sb = new StringBuffer();
+
+    StringTokenizer st = new StringTokenizer(br.readLine());
+
+    V = Integer.parseInt(st.nextToken());
+    E = Integer.parseInt(st.nextToken());
+    start = Integer.parseInt(br.readLine());
+    graph = new ArrayList[V + 1];
+    dist = new int[V + 1];
+
+    // graph 초기화
+    for(int i = 0; i <= V; i++) {
+      graph[i] = new ArrayList<>();
+    }
+
+    // dist 초기화
+    Arrays.fill(dist, Integer.MAX_VALUE);
+    dist[start] = 0;
+
+    for(int i = 0; i < E; i++) {
+      st = new StringTokenizer(br.readLine());
+      int u = Integer.parseInt(st.nextToken());
+      int v = Integer.parseInt(st.nextToken());
+      int w = Integer.parseInt(st.nextToken());
+      graph[u].add(new Node(v, w));
+    }
+
+    PriorityQueue<Node> pq = new PriorityQueue<>((o1, o2) -> o1.cost - o2.cost);
+    pq.offer(new Node(start, 0));
+
+    while(!pq.isEmpty()) {
+      Node cur = pq.poll();
+      int curIndex = cur.index;
+      int curCost = cur.cost;
+
+      if(dist[curIndex] < curCost) continue;
+
+      for(int i = 0; i < graph[curIndex].size(); i++) {
+        Node next = graph[curIndex].get(i);
+        int nextIndex = next.index;
+        int nextCost = next.cost;
+
+        if(dist[nextIndex] > curCost + nextCost) {
+          dist[nextIndex] = curCost + nextCost;
+          pq.offer(new Node(nextIndex, dist[nextIndex]));
         }
-	}
-	
+      }
+    }
+
+    for(int i = 1; i <= V; i++) {
+      if(dist[i] == Integer.MAX_VALUE) {
+        System.out.println("INF");
+      } else {
+        System.out.println(dist[i]);
+      }
+    }
+
+    bw.write(sb.toString());
+    bw.flush();
+    bw.close();
+  }
+
 }

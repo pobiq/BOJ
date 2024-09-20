7d2d70a3054fbadf5522a3e260ef5a4c80abd111
junghoon <111417407+pobiq@users.noreply.github.com>
2024-09-20 17:23:15 +0900

    [Gold IV] Title: 최소 스패닝 트리, Time: 500 ms, Memory: 49308 KB -BaekjoonHub

--- 백준/Gold/1197. 최소 스패닝 트리/최소 스패닝 트리.java
+++ 백준/Gold/1197. 최소 스패닝 트리/최소 스패닝 트리.java
@@ -3,94 +3,89 @@ import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.PriorityQueue;
-import java.util.StringTokenizer;
+import java.util.*;
 
 public class Main {
-	
-	static class Edge implements Comparable<Edge> {
-        int to;
-        int weight;
-        
-        public Edge(int to, int weight) {
-            this.to = to;
-            this.weight = weight;
-        }
-        
-        @Override
-        public int compareTo(Edge o) {
-            return weight - o.weight;
-        }
-        
-        public String toString() {
-        	return this.to + " " + this.weight;
-        }
-    }
-	
-	static int V, E;
-    static ArrayList<ArrayList<Edge>> graph;
-    static PriorityQueue<Edge> pq;
-    static boolean[] visited;
-	
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-		StringBuffer sb = new StringBuffer();
-		
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		
-		V = Integer.parseInt(st.nextToken());
-		E = Integer.parseInt(st.nextToken());
-		visited = new boolean[V+1];
-		pq = new PriorityQueue<>();
-		graph = new ArrayList<>();
-		
-		long result = 0;
-		
-		for(int i=0; i<=V; i++) {
-			graph.add(new ArrayList<>());
-		}
-		
-		
-		for(int i=0; i<E; i++) {
-			st = new StringTokenizer(br.readLine());
-			int u = Integer.parseInt(st.nextToken());
-			int v = Integer.parseInt(st.nextToken());
-			int w = Integer.parseInt(st.nextToken());
-			graph.get(u).add(new Edge(v, w));
-			graph.get(v).add(new Edge(u, w));
-		}
-		
-		pq.add(new Edge(1, 0));
-		
-		while(!pq.isEmpty()) {
-			Edge edge = pq.poll();
-			int to = edge.to;
-			int weight = edge.weight;
-			
-			if(visited[to]) {
-				continue;
-			}
-			
-			visited[to] = true;
-			result += weight;
-			
-			for(Edge e : graph.get(to)) {
-				if(!visited[e.to]) {
-					pq.offer(e);
-				}
-			}
-		}
 
-		
-		sb.append(result);
-		
-		bw.write(sb.toString());
-		bw.flush();
-		bw.close();
-		
-	}
-	
+  static int total;
+  static int[] parent;
+  static class Node implements Comparable<Node>{
+    int to;
+    int from;
+    int value;
+
+    public Node(int to, int from, int value) {
+      this.to = to;
+      this.from = from;
+      this.value = value;
+    }
+
+    @Override
+    public int compareTo(Node o) {
+      return this.value - o.value;
+    }
+  }
+
+  public static void main(String[] args) throws IOException {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+    StringBuffer sb = new StringBuffer();
+
+    StringTokenizer st = new StringTokenizer(br.readLine());
+    int v = Integer.parseInt(st.nextToken());
+    int e = Integer.parseInt(st.nextToken());
+
+    parent = new int[v+1];
+    for(int i=1; i<v+1; i++) {
+      parent[i] = i;
+    }
+
+    Queue<Node> pq = new PriorityQueue<>();
+    for(int i=0; i<e; i++) {
+      st = new StringTokenizer(br.readLine());
+      int a = Integer.parseInt(st.nextToken());
+      int b = Integer.parseInt(st.nextToken());
+      int w = Integer.parseInt(st.nextToken());
+      pq.add(new Node(a,b,w));
+    }
+
+    int size = pq.size();
+    int total = 0;
+    for(int i=0; i<size; i++) {
+      Node node = pq.poll();
+      int to = find(node.to);
+      int from = find(node.from);
+
+      if(!isSameParent(to,from)) {
+        total += node.value;
+        union(node.to, node.from);
+      }
+    }
+
+    sb.append(total);
+    bw.write(sb.toString());
+    bw.flush();
+    bw.close();
+  }
+
+  public static int find(int x) {
+    if(parent[x] ==x ) return x;
+    return parent[x] = find(parent[x]);
+  }
+
+  public static void union(int x, int y) {
+    x = find(x);
+    y = find(y);
+    if(x!= y) {
+      parent[y] =x;
+    }
+  }
+
+  public static boolean isSameParent(int x, int y ) {
+    x = find(x);
+    y = find(y);
+    if(x==y) return true;
+    else return false;
+  }
+
 }

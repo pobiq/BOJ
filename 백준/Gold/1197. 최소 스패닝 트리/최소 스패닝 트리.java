e35678ced74bfb0e91106d1b59a272c10701d426
junghoon <111417407+pobiq@users.noreply.github.com>
2024-09-20 17:29:24 +0900

    [Gold IV] Title: 최소 스패닝 트리, Time: 484 ms, Memory: 49384 KB -BaekjoonHub

--- 백준/Gold/1197. 최소 스패닝 트리/최소 스패닝 트리.java
+++ 백준/Gold/1197. 최소 스패닝 트리/최소 스패닝 트리.java
@@ -3,26 +3,32 @@ import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
-import java.util.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.PriorityQueue;
+import java.util.StringTokenizer;
 
 public class Main {
 
   static int[] parent;
+  static int V, E;
+
   static class Node implements Comparable<Node>{
     int to;
     int from;
-    int value;
+    int weight;
 
-    public Node(int to, int from, int value) {
+    Node(int to, int from, int weight) {
       this.to = to;
       this.from = from;
-      this.value = value;
+      this.weight = weight;
     }
 
     @Override
     public int compareTo(Node o) {
-      return this.value - o.value;
+      return this.weight - o.weight;
     }
+
   }
 
   public static void main(String[] args) throws IOException {
@@ -30,35 +36,35 @@ public class Main {
     BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
     StringBuffer sb = new StringBuffer();
 
+    StringTokenizer st = new StringTokenizer(br.readLine());
+
     int result = 0;
 
-    StringTokenizer st = new StringTokenizer(br.readLine());
-    int v = Integer.parseInt(st.nextToken());
-    int e = Integer.parseInt(st.nextToken());
+    V = Integer.parseInt(st.nextToken());
+    E = Integer.parseInt(st.nextToken());
+    parent = new int[V + 1];
 
-    parent = new int[v+1];
-    for(int i=1; i<v+1; i++) {
+    // parent 초기화
+    for(int i = 1; i <= V; i++) {
       parent[i] = i;
     }
 
-    Queue<Node> pq = new PriorityQueue<>();
-    for(int i=0; i<e; i++) {
+    PriorityQueue<Node> pq = new PriorityQueue<>();
+
+    for(int i = 0; i < E; i++) {
       st = new StringTokenizer(br.readLine());
-      int a = Integer.parseInt(st.nextToken());
-      int b = Integer.parseInt(st.nextToken());
-      int w = Integer.parseInt(st.nextToken());
-      pq.add(new Node(a,b,w));
+      int to = Integer.parseInt(st.nextToken());
+      int from = Integer.parseInt(st.nextToken());
+      int weight = Integer.parseInt(st.nextToken());
+      pq.offer(new Node(to, from, weight));
     }
 
     int size = pq.size();
-    int total = 0;
-    for(int i=0; i<size; i++) {
-      Node node = pq.poll();
-      int to = find(node.to);
-      int from = find(node.from);
 
-      if(!isSameParent(to,from)) {
-        result += node.value;
+    for(int i = 0; i < size; i++) {
+      Node node = pq.poll();
+      if(!isSameParent(node.to, node.from)) {
+        result += node.weight;
         union(node.to, node.from);
       }
     }
@@ -69,24 +75,23 @@ public class Main {
     bw.close();
   }
 
-  public static int find(int x) {
-    if(parent[x] ==x ) return x;
+  private static boolean isSameParent(int to, int from) {
+    int x = find(to);
+    int y = find(from);
+    return x == y;
+  }
+
+  private static int find(int x) {
+    if(parent[x] == x) return x;
     return parent[x] = find(parent[x]);
   }
 
-  public static void union(int x, int y) {
-    x = find(x);
-    y = find(y);
-    if(x!= y) {
-      parent[y] =x;
+  private static void union(int to, int from) {
+    int x = find(to);
+    int y = find(from);
+    if(x != y) {
+      parent[y] = x;
     }
-  }
-
-  public static boolean isSameParent(int x, int y ) {
-    x = find(x);
-    y = find(y);
-    if(x==y) return true;
-    else return false;
   }
 
 }

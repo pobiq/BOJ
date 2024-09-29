e45ee3af5f379199577b314307f258fd50d3c194
junghoon <111417407+pobiq@users.noreply.github.com>
2024-09-29 21:42:25 +0900

    [level 2] Title: 방문 길이, Time: 2.32 ms, Memory: 77.3 MB -BaekjoonHub

---
+++ 프로그래머스/2/49994. 방문 길이/방문 길이.java
@@ -0,0 +1,84 @@
+import java.util.*;
+
+class Solution {
+  public class Node {
+    int startX;
+    int startY;
+    int endX;
+    int endY;
+
+    Node(int startX, int startY, int endX, int endY) {
+      this.startX = startX;
+      this.startY = startY;
+      this.endX = endX;
+      this.endY = endY;
+    }
+
+    public String toString() {
+      return "(" + startX + ", " + startY + ", " + endX + ", " + endY + ")";
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+      if(this == obj) {
+        return true;
+      }
+      if(obj == null) {
+        return false;
+      }
+      if(getClass() != obj.getClass()) {
+        return false;
+      }
+      Node node = (Node) obj;
+      return this.startX == node.startX &&
+          this.startY == node.startY &&
+          this.endX == node.endX &&
+          this.endY == node.endY;
+    }
+
+    @Override
+    public int hashCode() {
+      return Objects.hash(startX, startY, endX, endY);
+    }
+  }
+
+  public int solution(String dirs) {
+    HashSet<Node> hs = new HashSet<>();
+    int startX = 5;
+    int startY = 5;
+    int endX = 5;
+    int endY = 5;
+
+    for(int i = 0; i < dirs.length(); i++) {
+      startX = endX;
+      startY = endY;
+      char c = dirs.charAt(i);
+
+      switch (c) {
+        case 'U':
+          if(startX == 0) continue;
+          endX -= 1;
+          break;
+        case 'D':
+          if(startX == 10) continue;
+          endX += 1;
+          break;
+        case 'R':
+          if(startY == 10) continue;
+          endY += 1;
+          break;
+        case 'L':
+          if(startY == 0) continue;
+          endY -= 1;
+          break;
+      }
+
+      Node temp = new Node(endX, endY, startX, startY);
+      if(hs.contains(temp)) continue;
+
+      hs.add(new Node(startX, startY, endX, endY));
+    }
+    
+    return hs.size();
+  }
+}

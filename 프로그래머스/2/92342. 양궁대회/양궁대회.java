44289789ccd74629f33c80c97cbddc4c6b445e87
junghoon <111417407+pobiq@users.noreply.github.com>
2025-04-09 14:48:05 +0900

    [level 2] Title: 양궁대회, Time: 0.96 ms, Memory: 74 MB -BaekjoonHub

---
+++ 프로그래머스/2/92342. 양궁대회/양궁대회.java
@@ -0,0 +1,74 @@
+public class Solution {
+
+  static int[] answer = new int[11];
+
+  static int maxDiff = 0;
+
+  public int[] solution(int n, int[] info) {
+
+    int[] output = new int[11];
+
+    recursive(n, info, output, 0, 0);
+
+    if(maxDiff == 0) {
+      answer = new int[]{-1};
+    }
+
+    return answer;
+  }
+
+  private void recursive(int n, int[] info, int[] output, int index, int k) {
+    if(index == 11) {
+
+      // 남은 화살 0점에 쏘기
+      if(k < n) {
+        output[10] = n - k;
+      }
+      result(info, output);
+      return;
+    }
+
+    // 1. 어피치를 이길때
+    if(n - k > info[index]) {
+      output[index] = info[index] + 1;
+      recursive(n, info, output, index+1, k + info[index] + 1);
+    }
+
+    // 2. 안쏘고 넘어갈때
+    output[index] = 0;
+    recursive(n, info, output, index+1, k);
+
+  }
+
+  private void result(int[] info, int[] output) {
+    int apeach_score = 0;
+    int lion_score = 0;
+
+    for(int i = 0; i < 11; i++) {
+      if(info[i] == 0 && output[i] == 0) continue;
+
+      if(info[i] >= output[i]) {
+        apeach_score += 10 - i;
+      } else if(info[i] < output[i]){
+        lion_score += 10 - i;
+      }
+    }
+
+    int diff = lion_score - apeach_score;
+    
+    if(maxDiff < diff) {
+      maxDiff = diff;
+      answer = output.clone();
+    } else if(maxDiff == diff) {
+      for(int i = 10; i >= 0; i--) {
+        if(info[i] < output[i]) {
+          answer = output.clone();
+          break;
+        } else if(answer[i] > output[i]) return;
+      }
+    }
+
+  }
+
+
+}

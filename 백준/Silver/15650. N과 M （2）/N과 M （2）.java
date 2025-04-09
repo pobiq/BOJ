56723e472fdc85216fb141d0903eedb0e0d881f4
junghoon <111417407+pobiq@users.noreply.github.com>
2025-04-09 17:24:10 +0900

    [Silver III] Title: N과 M (2), Time: 128 ms, Memory: 16104 KB -BaekjoonHub

--- 백준/Silver/15650. N과 M （2）/N과 M （2）.java
+++ 백준/Silver/15650. N과 M （2）/N과 M （2）.java
@@ -4,44 +4,45 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 import java.util.StringTokenizer;
+import java.util.stream.IntStream;
 
 public class Main {
-	
-	static int n,m;
-	static int[] arr;
-	
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-		StringBuffer sb = new StringBuffer();
-		
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		
-		n = Integer.parseInt(st.nextToken());
-		m = Integer.parseInt(st.nextToken());
-		
-		arr = new int[m];
-		
-		dfs(1, 0);
-		
-		bw.write(sb.toString());
-		bw.flush();
-		bw.close();
-	}
-	
-	public static void dfs(int start, int depth) {
-		if(depth == m) {
-			for(int i : arr) {
-				System.out.print(i + " ");
-			}
-			System.out.println();
-			return;
-		}
-		
-		for(int i=start; i<=n; i++) {
-			arr[depth] = i;
-			dfs(i+1, depth+1);
-		}
-	}
-	
+
+
+  public static void main(String[] args) throws IOException {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+    StringBuffer sb = new StringBuffer();
+
+    StringTokenizer st = new StringTokenizer(br.readLine());
+
+    int n = Integer.parseInt(st.nextToken());
+    int m = Integer.parseInt(st.nextToken());
+
+    int[] input = IntStream.rangeClosed(1, n).toArray();
+    int[] output = new int[m];
+
+    combination(input, output, 0, 0, m);
+
+    bw.write(sb.toString());
+    bw.flush();
+    bw.close();
+  }
+
+  private static void combination(int[] input, int[] output, int depth, int start, int m) {
+    if(depth == m) {
+      for(int number : output) {
+        System.out.print(number + " ");
+      }
+      System.out.println();
+      return;
+    }
+
+    for(int i = start; i < input.length; i++) {
+      output[depth] = input[i];
+      combination(input, output, depth+1, i+1, m);
+    }
+
+  }
+
 }

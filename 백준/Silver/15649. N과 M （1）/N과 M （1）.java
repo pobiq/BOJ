a9d05e2c44ea33be935fee46ca74a45b4e022309
junghoon <111417407+pobiq@users.noreply.github.com>
2025-04-09 16:28:06 +0900

    [Silver III] Title: N과 M (1), Time: 2436 ms, Memory: 89372 KB -BaekjoonHub

--- 백준/Silver/15649. N과 M （1）/N과 M （1）.java
+++ 백준/Silver/15649. N과 M （1）/N과 M （1）.java
@@ -3,53 +3,57 @@ import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.PriorityQueue;
 import java.util.StringTokenizer;
+import java.util.stream.IntStream;
 
 public class Main {
-	
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-		StringBuffer sb = new StringBuffer();
-		
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		
-		int n = Integer.parseInt(st.nextToken());
-		int m = Integer.parseInt(st.nextToken());
-		
-		
-		int[] arr = new int[n];
-		int[] out = new int[m];
-		boolean[] visited = new boolean[n];
-		
-		for(int i=0; i<n; i++) {
-			arr[i] = i+1;
-		}
-		
-		permutation(arr, out, visited, 0, m);
-		
-		bw.write(sb.toString());
-		bw.flush();
-		bw.close();
-	}
-	
-	public static void permutation(int[] arr, int[] out, boolean[] visited, int depth, int r) {
-		if(depth == r) {
-			for(int i : out) {
-				System.out.print(i + " ");
-			}
-			System.out.println();
-			return;
-		}
-		
-		for(int i=0; i<arr.length; i++) {
-			if(!visited[i]) {
-				visited[i] = true;
-				out[depth] = arr[i];
-				permutation(arr, out, visited, depth+1, r);
-				visited[i] = false;
-			}
-		}
-	}
-	
+
+
+  public static void main(String[] args) throws IOException {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+    StringBuffer sb = new StringBuffer();
+
+    StringTokenizer st = new StringTokenizer(br.readLine());
+
+    int n = Integer.parseInt(st.nextToken());
+    int m = Integer.parseInt(st.nextToken());
+
+    int[] input = IntStream.rangeClosed(1, n).toArray();
+    int[] output = new int[m];
+    boolean[] visited = new boolean[n];
+
+    permutation(input, output, visited, 0, m);
+
+    bw.write(sb.toString());
+    bw.flush();
+    bw.close();
+  }
+
+  private static void permutation(int[] input, int[] output,
+      boolean[] visited, int depth, int m) {
+    if(depth == m) {
+
+      for(int number : output) {
+        System.out.print(number + " ");
+      }
+      System.out.println();
+
+      return;
+    }
+
+    for(int i = 0; i < input.length; i++) {
+      if(!visited[i]) {
+        visited[i] = true;
+        output[depth] = input[i];
+        permutation(input, output, visited,depth+1, m);
+        visited[i] = false;
+      }
+    }
+
+  }
+
 }

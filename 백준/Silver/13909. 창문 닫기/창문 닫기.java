26e473dd1a7266c9227400b8bc01e2ee88fe443f
junghoon <111417407+pobiq@users.noreply.github.com>
2025-05-28 14:40:58 +0900

    [Silver V] Title: 창문 닫기, Time: 100 ms, Memory: 14176 KB -BaekjoonHub

---
+++ 백준/Silver/13909. 창문 닫기/창문 닫기.java
@@ -0,0 +1,24 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+public class Main {
+
+  public static void main(String[] args) throws IOException {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+    StringBuilder sb = new StringBuilder();
+
+    int n = Integer.parseInt(br.readLine());
+    int answer = (int) Math.sqrt(n);
+
+    sb.append(answer);
+
+    bw.write(sb.toString());
+    bw.flush();
+    bw.close();
+  }
+
+}

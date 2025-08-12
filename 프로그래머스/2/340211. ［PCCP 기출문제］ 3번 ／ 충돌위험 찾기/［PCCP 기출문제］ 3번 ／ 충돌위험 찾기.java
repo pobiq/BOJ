d97b51979299a0b0c181b1e0cd134c3b775166b3
junghoon <111417407+pobiq@users.noreply.github.com>
2025-08-12 23:45:59 +0900

    [level 2] Title: [PCCP 기출문제] 3번 / 충돌위험 찾기, Time: 763.47 ms, Memory: 1.06 GB -BaekjoonHub

--- 프로그래머스/2/340211. ［PCCP 기출문제］ 3번 ／ 충돌위험 찾기/［PCCP 기출문제］ 3번 ／ 충돌위험 찾기.java
+++ 프로그래머스/2/340211. ［PCCP 기출문제］ 3번 ／ 충돌위험 찾기/［PCCP 기출문제］ 3번 ／ 충돌위험 찾기.java
@@ -1,86 +1,57 @@
-import java.util.HashMap;
-
 class Solution {
-    public class Node {
-        int x;
-        int y;
-        int time;
-
-        public Node(int x, int y, int time) {
-            this.x = x;
-            this.y = y;
-            this.time = time;
-        }
-        
-        @Override
-        public boolean equals(Object o) {
-            if (this == o) return true; // 같은 객체면 true
-            if (o == null || getClass() != o.getClass()) return false;
-            Node node = (Node) o;
-            return x == node.x && y == node.y && time == node.time;
-        }
-
-        @Override
-        public int hashCode() {
-            return java.util.Objects.hash(x, y, time);
-        }
-        
-    }
-
+    
     public int solution(int[][] points, int[][] routes) {
         int answer = 0;
+        
+        int[][][] map = new int[20000][100][100];
 
-        HashMap<Node, Integer> hm = new HashMap<>();
+        int n = points.length;
+        int m = routes.length;
 
-        for (int i = 0; i < routes.length; i++) {
-            int time = 0;
-            for (int j = 1; j < routes[i].length; j++) {
-                int start = routes[i][j - 1] - 1;
-                int end = routes[i][j] - 1;
-                int startX = points[start][0] - 1;
-                int startY = points[start][1] - 1;
-                int endX = points[end][0] - 1;
-                int endY = points[end][1] - 1;
+        for(int i = 0; i < m; i++) {
+          int time = 0;
+          for(int j = 1; j < routes[i].length; j++) {
+            int start = routes[i][j-1] - 1;
+            int end = routes[i][j] - 1;
+            int startX = points[start][0] - 1;
+            int startY = points[start][1] - 1;
+            int endX = points[end][0] - 1;
+            int endY = points[end][1] - 1;
 
-                while (true) {
+            while(true) {
 
-                    if (startX == endX && startY == endY) {
-                        break;
-                    }
+              if(startX == endX && startY == endY) {
+                break;
+              }
 
-                    Node now = new Node(startX, startY, time);
-
-                    hm.put(now, hm.getOrDefault(now, 0) + 1);
-
-                    if (startX < endX) {
-                        startX++;
-                    } else if (startX > endX) {
-                        startX--;
-                    } else {
-                        if (startY < endY) {
-                            startY++;
-                        } else if (startY > endY) {
-                            startY--;
-                        }
-                    }
-                    time++;
-                }
-
-                Node now = new Node(startX, startY, time);
-
-
-                if (j == routes[i].length - 1) {
-                    hm.put(now, hm.getOrDefault(now, 0) + 1);
-                }
-            }
-        }
-
-        for (int value : hm.values()) {
-            if(value >= 2) {
+              map[time][startX][startY]++;
+              if(map[time][startX][startY] == 2) {
                 answer++;
-            }
-        }
+              }
 
+              if(startX < endX) {
+                startX++;
+              } else if(startX > endX) {
+                startX--;
+              } else {
+                if(startY < endY) {
+                  startY++;
+                } else if(startY > endY) {
+                  startY--;
+                }
+              }
+              time++;
+            }
+
+            if(j == routes[i].length - 1) {
+              map[time][startX][startY]++;
+              if(map[time][startX][startY] == 2) {
+                answer++;
+              }
+            }
+          }
+        }
+        
         return answer;
     }
 }

16524f068721a78190159d3aae4ca06883f293fe
junghoon <111417407+pobiq@users.noreply.github.com>
2025-08-13 12:43:16 +0900

    [Bronze V] Title: 문자와 문자열, Time: 104 ms, Memory: 14156 KB -BaekjoonHub

--- 백준/Bronze/27866. 문자와 문자열/문자와 문자열.java
+++ 백준/Bronze/27866. 문자와 문자열/문자와 문자열.java
@@ -1,26 +1,19 @@
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
-import java.util.StringTokenizer;
+import java.io.*;
 
 public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringBuilder sb = new StringBuilder();
+        String str = br.readLine();
+        int index = Integer.parseInt(br.readLine());
 
-  public static void main(String[] args) throws IOException {
-    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-    BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-    StringBuilder sb = new StringBuilder();
+        sb.append(str.charAt(index-1));
 
-    String s = br.readLine();
-    int i = Integer.parseInt(br.readLine());
-    char c = s.charAt(i-1);
+        bw.write(sb.toString());
+        bw.flush();
+        bw.close();
+        br.close();
 
-    sb.append(c);
-
-    bw.write(sb.toString());
-    bw.flush();
-    bw.close();
-  }
-
+    }
 }

c948dfb0f165c1419385e0637ccbc0323192810d
junghoon <111417407+pobiq@users.noreply.github.com>
2025-09-04 14:03:25 +0900

    [level 2] Title: 최댓값과 최솟값, Time: 0.06 ms, Memory: 9.44 MB -BaekjoonHub

---
+++ 프로그래머스/2/12939. 최댓값과 최솟값/최댓값과 최솟값.py
@@ -0,0 +1,13 @@
+def solution(s):
+    answer = ''
+    max_number = float("inf") * -1
+    min_number = float("inf")
+    
+    for num in s.split(" "):
+        int_num = int(num)
+        max_number = max(max_number, int_num)
+        min_number = min(min_number, int_num)
+
+    answer = str(min_number) + " " + str(max_number)
+    
+    return answer

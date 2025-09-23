58ce3e3adc58172d54879517702c16542c9bf999
junghoon <111417407+pobiq@users.noreply.github.com>
2025-09-23 15:40:51 +0900

    [level 2] Title: 타겟 넘버, Time: 394.22 ms, Memory: 9.2 MB -BaekjoonHub

--- 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.py
+++ 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.py
@@ -1,30 +1,14 @@
-from collections import deque
-
-
+answer = 0
 def solution(numbers, target):
-    answer = 0
-    answer = BFS(numbers, target)
+    dfs(numbers, 0, 0, target, len(numbers))
     return answer
 
-def BFS(numbers, target):
-    result = 0
-
-    length = len(numbers)
-
-    queue = deque()
-    queue.append([numbers[0], 0])
-    queue.append([-1 * numbers[0], 0])
-
-    while queue:
-        temp = queue.popleft()
-        sum = temp[0]
-        index = temp[1]
-
-        if index == length-1:
-            if sum == target:
-                result += 1
-        else:
-            queue.append([sum + numbers[index+1], index+1])
-            queue.append([sum + -1 * numbers[index+1], index+1])
-
-    return result
+def dfs(numbers, total, depth, target, length):
+    global answer
+    if depth == length:
+        if total == target:
+            answer += 1
+        return
+    
+    dfs(numbers, total+numbers[depth], depth + 1, target, length)
+    dfs(numbers, total-numbers[depth], depth + 1, target, length)

7ccedac206a9c77e60e99a32fa743db061066744
junghoon <111417407+pobiq@users.noreply.github.com>
2026-06-04 19:34:06 +0900

    [level 0] Title: 공백으로 구분하기 1, Time: 8.18 ms, Memory: 61.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/181869. 공백으로 구분하기 1/공백으로 구분하기 1.kt
@@ -0,0 +1,7 @@
+class Solution {
+    fun solution(my_string: String): List<String> {
+        var answer: List<String> = listOf<String>()
+        answer = my_string.split(" ")
+        return answer
+    }
+}
